c2d64eb55e1c34cbd67ef9f3bd52e54d00f13723
Robert Irelan <rirelan@gmail.com>
2024-03-14 18:05:27 -0700

    generate_data.sh: make able to run from any directory

--- excess_return_scripts/generate_data.sh
+++ excess_return_scripts/generate_data.sh
@@ -2,8 +2,9 @@
 
 set -eu -o pipefail
 set -xv
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Script expects to be in folder that contains PDFs of earning call transcripts" 1>&2
 for x in *.pdf; do pdftotext "$x"; done
-bash -xv ./extract_date.sh > stock_quarterly_call_dates.csv
-python3 download_stock_date.py stock_quarterly_call_dates.csv data_output.csv return_output.csv
+bash -xv "$script_dir/extract_date.sh" > stock_quarterly_call_dates.csv
+python3 "$script_dir/download_stock_date.py" stock_quarterly_call_dates.csv data_output.csv return_output.csv
